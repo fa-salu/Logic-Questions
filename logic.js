ec57dff73f8ba64ebd4ee7453948fc7f1be8e589
fasalu <fasalurahman7515@gmail.com>
2024-07-11 20:26:45 +0530

    new qeustion

--- logic.js
+++ logic.js
@@ -30,3 +30,17 @@ const isEqualTo100 = (a, b) => a === 100 || b === 100 || (a + b) === 100;
 console.log(isEqualTo100(100, 0));  //true
 console.log(isEqualTo100(50, 30));  //false
 console.log(isEqualTo100(80, 20));  //true
+
+// Que - 5
+// First letter Change to UpperCase
+
+function names(nam) {
+  let word = nam.split(' ');
+  for (i = 0; i < word.length; i++) {
+      word[i] = word[i][0].toUpperCase() + word[i].slice(1).toLowerCase();
+  }
+  res = word.join(' ');
+  return res;
+}
+
+console.log(names('my name is fazal'));  // My Name Is Fazal
