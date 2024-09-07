0045f5b4f2a32a161fe01eebfd47fe70d6f05493
fasalu <fasalurahman7515@gmail.com>
2024-09-07 21:57:04 +0530

    add another logic question

--- logic.js
+++ logic.js
@@ -3,7 +3,7 @@
 
 const largestEven = (arr) => Math.max(...arr.filter((num) => num % 2 === 0));
 
-console.log(largestEven([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));  //output - 10
+console.log(largestEven([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])); //output - 10
 
 // Que - 2
 // find Greaterthan or equal four letter words
@@ -12,7 +12,7 @@ function fourLetter(name) {
   return name.filter((name) => name.length >= 4);
 }
 
-console.log(fourLetter(["Tomato", "Pair", "Air"]));  // output - ['Tomato', 'Pair']
+console.log(fourLetter(["Tomato", "Pair", "Air"])); // output - ['Tomato', 'Pair']
 
 //Que - 3
 // find legs of 2 chicken, 3 cow and 5 pigs
@@ -21,26 +21,52 @@ function animalsLegs(chickens, cow, pigs) {
   return chickens * 2 + cow * 4 + pigs * 4;
 }
 
-console.log(animalsLegs(2, 3, 5));  // output - 36
+console.log(animalsLegs(2, 3, 5)); // output - 36
 
 //Que - 4
 //check two numbers and return true if the number is 100 or if the sum of the two numbers is 100
 
-const isEqualTo100 = (a, b) => a === 100 || b === 100 || (a + b) === 100;
-console.log(isEqualTo100(100, 0));  //true
-console.log(isEqualTo100(50, 30));  //false
-console.log(isEqualTo100(80, 20));  //true
+const isEqualTo100 = (a, b) => a === 100 || b === 100 || a + b === 100;
+console.log(isEqualTo100(100, 0)); //true
+console.log(isEqualTo100(50, 30)); //false
+console.log(isEqualTo100(80, 20)); //true
 
 // Que - 5
 // First letter Change to UpperCase
 
 function names(nam) {
-  let word = nam.split(' ');
+  let word = nam.split(" ");
   for (i = 0; i < word.length; i++) {
-      word[i] = word[i][0].toUpperCase() + word[i].slice(1).toLowerCase();
+    word[i] = word[i][0].toUpperCase() + word[i].slice(1).toLowerCase();
   }
-  res = word.join(' ');
+  res = word.join(" ");
   return res;
 }
 
-console.log(names('my name is fazal'));  // My Name Is Fazal
+console.log(names("my name is fazal")); // My Name Is Fazal
+
+
+// Que -6
+// input = 'abc'
+// output = [a,b,c,ab,bc,abc]
+
+const input = "abc";
+
+function val(input) {
+  const arr = [];
+
+  for (let i = 0; i < input.length; i++) {
+    for (let j = i + 1; j <= input.length; j++) {
+      const values = input.slice(i, j);
+      // console.log(values);
+
+      arr.push(values);
+    }
+  }
+
+  arr.sort((a, b) => a.length - b.length);
+  return arr;
+}
+
+const result = val(input);
+console.log(result);
