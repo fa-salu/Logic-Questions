4a4e7f18c01807956cbb8205fa4b14a195229024
Fasalu Rahman <145885597+fa-salu@users.noreply.github.com>
2024-10-17 18:51:54 +0530

    string- valid parantheses

--- leetCode.js
+++ leetCode.js
@@ -63,3 +63,28 @@ var plusOne = function(digits) {
     let inc = String(BigInt(res) + 1n)
     return inc.toString().split('').map(Number)
 };
+
+
+// string 20. Valid Parentheses
+
+var isValid = function(s) {   
+    const stack = [];
+    
+    for (let i = 0 ; i < s.length ; i++) {
+        let c = s.charAt(i);
+        switch(c) {
+            case '(': stack.push(')')
+            break;
+            case '[': stack.push(']')
+                break;
+            case '{': stack.push('}')
+                break;
+            default:
+                if (c !== stack.pop()) {
+                    return false
+                }
+        }
+    }
+    
+    return stack.length === 0
+};
