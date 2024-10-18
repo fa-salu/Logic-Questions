3dcaa8050826eb855a549fd89054f778c705ca14
Fasalu Rahman <145885597+fa-salu@users.noreply.github.com>
2024-10-18 10:42:42 +0530

    Update leetCode.js

--- leetCode.js
+++ leetCode.js
@@ -88,3 +88,20 @@ var isValid = function(s) {
     
     return stack.length === 0
 };
+
+
+// 14. Longest Common Prefix
+
+var longestCommonPrefix = function (strs) {
+  let prefix = strs[0];
+
+  for (let i = 1; i < strs.length; i++) {
+    while (strs[i].indexOf(prefix) !== 0) {
+      prefix = prefix.substring(0, prefix.length - 1);
+      if (prefix === "") {
+        return "";
+      }
+    }
+  }
+  return prefix;
+};
