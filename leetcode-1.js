0e06ac4f407776c7109b1bcc7661d3eea7800974
Fasalu Rahman <145885597+fa-salu@users.noreply.github.com>
2024-11-09 11:35:34 +0530

    Update leetcode-1.js

--- leetcode-1.js
+++ leetcode-1.js
@@ -26,3 +26,18 @@ ar longestPalindrome = function (s) {
 
   return longest;
 };
+
+
+// 83. Remove Duplicates from Sorted List
+
+var deleteDuplicates = function (head) {
+    let current = head;
+    while (current !== null && current.next !== null) {
+        if (current.val === current.next.val) {
+            current.next = current.next.next;
+        } else {
+            current = current.next;
+        }
+    }
+    return head;
+};
