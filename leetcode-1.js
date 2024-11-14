209ac231046dab9363992b166240f821bacc78fe
Fasalu Rahman <145885597+fa-salu@users.noreply.github.com>
2024-11-14 16:10:26 +0530

    Update leetcode-1.js

--- leetcode-1.js
+++ leetcode-1.js
@@ -77,3 +77,26 @@ var isPowerOfFour = function (n) {
     }
     return false
 };
+
+
+// 371. Sum of Two Integers
+var inter = function (nums1, nums2) {
+  nums1.sort((a, b) => a - b);
+  nums2.sort((a, b) => a - b);
+  let i = 0,
+    j = 0;
+  let result = [];
+  while (i < nums1.length && j < nums2.length) {
+    if (nums1[i] === nums2[j]) {
+      result.push(nums1[i]);
+      i++;
+      j++;
+    } else if (nums1[i] < nums2[j]) {
+      i++;
+    } else {
+      j++;
+    }
+  }
+  return result;
+};
+console.log(inter([2, 5, 3], [5, 7, 2]));
