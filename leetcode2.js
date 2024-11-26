b14c0d6d0a87e187606bfb319483e3f9aee92ab4
fa-salu <fasalurahman7515@gmail.com>
2024-11-26 14:47:13 +0530

    sorted color leetcode questionn

--- leetcode2.js
+++ leetcode2.js
@@ -16,3 +16,33 @@ var isHappy = function (n) {
 
   return n === 1;
 };
+
+//  75. Sort Colors
+
+var sortColors = function (nums) {
+  let count = 0,
+    count1 = 0,
+    count2 = 0;
+  for (let num of nums) {
+    if (num === 0) count++;
+    else if (num === 1) count1++;
+    else if (num === 2) count2++;
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    if (count > 0) {
+      nums[i] = 0;
+      count--;
+    } else if (count1 > 0) {
+      nums[i] = 1;
+      count1--;
+    } else {
+      nums[i] = 2;
+      count2--;
+    }
+  }
+
+  return nums
+
+};
+console.log(sortColors([2,0,1])); // [0,0,1,1,2,2]
