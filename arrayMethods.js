3becf127c84a4e7d86becaaa68caa8ea41076248
Fasalu Rahman <145885597+fa-salu@users.noreply.github.com>
2025-03-23 21:33:47 +0530

    Update arrayMethods.js

--- arrayMethods.js
+++ arrayMethods.js
@@ -72,3 +72,13 @@ function pr(nums) {
    return  s
 }
 
+// var titleToNumber = function(columnTitle) {
+    let result = 0;
+    
+    for (let i = 0; i < columnTitle.length; i++) {
+        let charValue = columnTitle.charCodeAt(i) - 'A'.charCodeAt(0) + 1;
+        result = result * 26 + charValue;
+    }
+    
+    return result;
+};
